ea4061ee7810ddeed70b69fc9106d07bf891ba7c
MihaelaBoian <pinzari_m@mail.ru>
2023-10-20 14:36:35 +0300

    P

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Parallelogram p1 = new Parallelogram();
+        p1.calculatePerimeter(4, 5);
+
+        Parallelogram p2 = new Parallelogram();
+        p2.calculatePerimeter(8);
+
+
+        Parallelogram a1=new Parallelogram();
+        a1.calculateArea(2,2.2);
+
+        Parallelogram a2=new Parallelogram();
+        a2.calculateArea(2);
+
     }
 }
--- src/Parallelogram.java
+++ src/Parallelogram.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class Parallelogram {
+public class Parallelogram {
+   int p;
+//    int width;
+//    int sideLength;
+
+    int calculatePerimeter(int length, int width) {
+    int perimetru=2*(length+width);
+                System.out.println("Length este = "+length+"  Width este= "+width+ "  Perimetrul calculat este:   "+perimetru);
+return perimetru;
+    }
+        int calculatePerimeter(int sideLength) {
+        int p=6*(sideLength);
+            System.out.println("Perimetrul este:   "+p);
+        return p;
+
+        }
+
+        double calculateArea(int length, double width) {
+            System.out.println("Length este = "+length+"  Width este= "+width+ "  Aria calculata este:   "+length*width);
+        return length*width;
+
+
+       }
+        int calculateArea(int sideLength) {
+        int Area=sideLength*5;
+        System.out.println("Aria calculata este:   "+Area);
+        return  Area;
+
+
+
+       }
+
 }
+//2.	Creați o clasă cu denumirea Parallelogram. Definiți următoarele 4 metode și logica corespunzătoare pentru calculul arie și al perimetrului:
+//
+//        int calculatePerimeter(int length, int width) { }
+//        int calculatePerimeter(int sideLength) { }--lungime latura
+//
+//        double calculateArea(int length, double width) { }
+//        int calculateArea(int sideLength) { }
+//
+//        Într-o clasă care are metoda main, instanțiați obiecte de tipul clasei Parallelogram, apelați metodele de calcul al ariei și perimetrului. Asigurați-vă că rezultatul invocării acestor metode este corect.
